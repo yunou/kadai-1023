8da25dff873e255ef71d9225591467176e008f08
yunou <Kairi04woody@gmail.com>
2019-10-23 05:31:50 +0000

    HTMLを追加

---
+++ kadai2/kadai.js
@@ -0,0 +1,14 @@
+const firstname = 'Kairi'
+const secondname = 'Uno'
+console.log(firstname + secondname)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+//
+console.log(!hasDog)
